4736ad43804b86c5e5f575d544ceffe4787e1150
amirhmi <amirhmi1377@gmail.com>
2020-07-03 09:53:54 +0430

    Add README file

--- src/Sender.java
+++ src/Sender.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class Sender {
     public static void main(String[] args) throws Exception {
         TCPSocket tcpSocket = new TCPSocketImpl(Config.localhost, Config.senderPort);
-        tcpSocket.send("resources/less.txt");
+        tcpSocket.send("resources/1MB.txt");
         tcpSocket.close();
         // tcpSocket.saveCongestionWindowPlot();
     }
